537b32e1303511c88f52b87d1f46e4d268e07d49
not2urs <68651645+not2urs@users.noreply.github.com>
2020-07-22 20:00:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("HAPPY BIRTHDAY ABHISHEK");
+function myFunction()
+{
+  document.querySelector("#ae").play();
+  document.querySelector("#aero").play();
+  document.querySelector("#aer").play();
+  document.querySelector("#aerop").play();
+  document.querySelector("#aeropl").play();
+  document.querySelector("#aeropla").play();
+  document.querySelector("#aeroplan").play();
+  document.querySelector("#aeroplane").play();
+
+}
